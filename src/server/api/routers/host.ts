b2bd699991b9b5b645b27f618a4407e329dc2111
Dominic Mostert <mostertdev@gmail.com>
2024-04-04 10:43:50 +0200

    Database & API

---
+++ src/server/api/routers/host.ts
@@ -0,0 +1,62 @@
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { z } from "zod";
+
+export const hostRouter = createTRPCRouter({
+  fetchHost: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+      include: {
+        packages: true,
+        bookings: true,
+      },
+    });
+  }),
+
+  fetchHostBySlug: publicProcedure
+    .input(
+      z.object({
+        hostSlug: z.string(),
+      }),
+    )
+    .query(({ input, ctx }) => {
+      return ctx.db.user.findUnique({
+        where: {
+          hostSlug: input.hostSlug,
+        },
+        include: {
+          packages: {
+            include: {
+              bookings: true,
+            },
+          },
+        },
+      });
+    }),
+
+  updateHost: protectedProcedure
+    .input(
+      z.object({
+        hostName: z.string(),
+        hostSlug: z.string(),
+        hostLogo: z.string(),
+      }),
+    )
+    .mutation(({ input, ctx }) => {
+      return ctx.db.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          hostName: input.hostName,
+          hostSlug: input.hostSlug,
+          hostLogo: input.hostLogo,
+        },
+      });
+    }),
+});
